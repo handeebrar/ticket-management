02d363778da8a8cb79ba226da5245493b394b86e
hgunesdogdu <hande.gunesdogdu@hepsiburada.com>
2021-04-26 00:18:51 +0300

    api integration tests added

---
+++ TicketManagement.Api.IntegrationTests/Controllers/CategoryControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TicketManagement.Api.IntegrationTests.Base;
+using TicketManagement.Application.Features.Categories.Queries.GetCategoriesList;
+using Xunit;
+
+namespace TicketManagement.Api.IntegrationTests.Controllers
+{
+    public class CategoryControllerTests : IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        private readonly CustomWebApplicationFactory<Startup> _factory;
+
+        public CategoryControllerTests(CustomWebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task ReturnsSuccessResult()
+        {
+            var client = _factory.GetAnonymousClient();
+            var response = await client.GetAsync("/api/category/all");
+
+            response.EnsureSuccessStatusCode();
+
+            var responseString = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<List<CategoryListViewModel>>(responseString);
+
+            Assert.IsType<List<CategoryListViewModel>>(result);
+            Assert.NotEmpty(result);
+        }
+    }
+}
